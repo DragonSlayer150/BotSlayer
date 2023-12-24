7edbab5920090e09286cb51ede536ac66b05c1c9
Ryan Kee <rjkee13@gmail.com>
2023-12-23 21:47:23 -0500

    removed unused code and added 'cheese' command

---
+++ src/commands/fun/cheese.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('cheese')
+		.setDescription('Replies with cheese!'),
+	async execute(interaction) {
+		await interaction.reply('Cheese');
+	},
+};
