9904e59856ed10945f52a4736317d1a4852c88c2
Ryan Kee <rjkee13@gmail.com>
2023-12-23 21:50:07 -0500

    ill get good at this I swear

--- src/events/join.js
+++ src/events/join.js
@@ -1,8 +1,8 @@
-const { Events, GuildMemberRoleManager, GuildManager } = require('discord.js');
+const { Events } = require('discord.js');
 
 module.exports = {
     name: Events.GuildMemberAdd,
-    async execute(member, client) {
+    async execute(member) {
         const role = member.guild.roles.cache.find(role => role.name == "The Slayers");
         member.roles.add(role);
     },
